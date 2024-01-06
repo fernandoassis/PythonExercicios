ad1258f4a66e95a363c3a13bde538e252cbf792e
Fernando Assis <fernandoassis.cloud@outlook.com>
2024-01-06 19:46:25 -0300

    Update ex003.py

--- ex003.py
+++ ex003.py
@@ -3,6 +3,8 @@
 num1=float(input('Digite o primeiro número da soma: '))
 num2=float(input('Digite o segundo número da soma: '))
 #solicita o número a ser digitado.
+#Obs: É necessário declarar o tipo primitivo para que a soma seja realizada corretamente...
+#... .Sem a declaração do tipo, a soma entenderá que são strings e apenas juntará os 2 números um ao lado do outro.
 
 soma=num1+num2
 #Faz a soma dos números digitados
