01cf2a848d2f85dffe5532025a8d1e0d47f05009
Fernando Assis <fernandoassis.cloud@outlook.com>
2024-01-06 19:34:42 -0300

    Atualização do exercício

--- ex003.py
+++ ex003.py
@@ -1,1 +1,12 @@
-#Exercício3
+#Solicite que digite 2 números e some os números
+
+num1=float(input('Digite o primeiro número da soma: '))
+num2=float(input('Digite o segundo número da soma: '))
+#solicita o número a ser digitado.
+
+soma=num1+num2
+#Faz a soma dos números digitados
+
+print(f'A soma entre o número {num1} e o número {num2} é: {soma}')
+#Escreve na tela o resultado da soma
+#Obs: f é usado para possibilitar que os números digitados sejam escritos no texto do resultado.
