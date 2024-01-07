2b6e016eb0fdbabd13c1098f7cf1873f38385c5a
Fernando Assis <fernandoassis.cloud@outlook.com>
2024-01-06 21:45:55 -0300

    Boas vindas

--- ex002.py
+++ ex002.py
@@ -1,6 +1,7 @@
 # Solicite o nome da pessoa e dê boas vindas
 
-nome=input('Digite seu nome:')
-#armazena o nome digitado.
+nome=input('Digite seu nome: ')
+#Lê e armazena o nome digitado.
 
 print('Bem vindo(a)', nome)
+#Imprime o nome digitado.
