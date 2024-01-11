5054a78d625bb946436efd0d0dce1401758cf828
Fernando Assis <fernandoassis.cloud@outlook.com>
2024-01-10 22:01:52 -0300

    Faça um programa que leia um número inteiro e mostre sua tabuada.

---
+++ ex009.py
@@ -0,0 +1,18 @@
+#Faça um programa que leia um número inteiro e mostre sua tabuada.
+#Solicita o número e armazena na variável
+n = int(input('Digite um número para ver sua tabuada: '))
+#Imprime - 20 vezes
+print('-'*20)
+#Imprime os resultados da tabuada na tela
+print('A tabuada de {} é:' .format(n))
+print('{} X  1 = {}' .format(n, n*1))
+print('{} X  2 = {}' .format(n, n*2))
+print('{} X  3 = {}' .format(n, n*3))
+print('{} X  4 = {}' .format(n, n*4))
+print('{} X  5 = {}' .format(n, n*5))
+print('{} X  6 = {}' .format(n, n*6))
+print('{} X  7 = {}' .format(n, n*7))
+print('{} X  8 = {}' .format(n, n*8))
+print('{} X  9 = {}' .format(n, n*9))
+print('{} X 10 = {}' .format(n, n*10))
+print('-'*20)
