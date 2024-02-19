31dd0075d8b9e6f9b806db9b8ea040a5da605d47
Fernando Assis <fernandoassis.cloud@outlook.com>
2024-02-19 11:48:48 -0300

    Abertura Star Wars

--- ex021.py
+++ ex021.py
@@ -3,16 +3,18 @@
 #Importa as bibliotecas pygame e time. Pode ser que precise instalar a biblioteca pygame
 import pygame
 import time
-#Inicia a biblioteca pygame
+
 pygame.init()
-#Dá o comando de ler o arquivo de música
-pygame.mixer.music.load('021.mp3')
-#Dá o play na música
-pygame.mixer.music.play()
-# Aguarde a duração da música antes de encerrar o programa
-while pygame.mixer.music.get_busy():
-    time.sleep(1)
-#Neste código, adicionamos um loop while que aguarda até que a música tenha terminado de tocar antes de encerrar o programa. 
-#O time.sleep(1) faz com que o programa aguarde 1 segundo antes de verificar se a música ainda está tocando. 
-#Isso garante que o programa aguarde a reprodução completa da música antes de encerrar o pygame
+pygame.mixer.init()
+pygame.mixer.music.load('ex021.mp3')
+
+# Define o número de vezes que você deseja repetir a música
+num_repeticoes = 1
+
+for _ in range(num_repeticoes):
+    pygame.mixer.music.play()
+    # Espera até que a música termine de tocar
+    while pygame.mixer.music.get_busy():
+        pygame.time.Clock().tick(10)  # Ajuste o FPS (frames por segundo) conforme necessário
+
 pygame.quit()
